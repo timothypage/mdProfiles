2e3b503909ea3327063b41d0054c95a7570d20e0
Tim Zwolak <timothypage@gmail.com>
2022-10-27 11:17:05 -0600

    add nggdpp profiles

--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@ import full from '../resources/profiles/full.json';
 import basic from '../resources/profiles/basic.json';
 import project from '../resources/profiles/project.json';
 import product from '../resources/profiles/product.json';
+import core from '../resources/profiles/core.json';
+import geologic from '../resources/profiles/geologic.json';
+import image from '../resources/profiles/image.json';
+import invertebrate from '../resources/profiles/invertebrate.json';
+import vertebrate from '../resources/profiles/vertebrate.json';
 
 import schema from '../resources/profile-schema.json';
 import pkg from '../package.json';
@@ -13,7 +18,30 @@ import pkg from '../package.json';
 const version = pkg.version;
 
 function asArray() {
-  return [full, basic, project, product];
+  return [
+    full,
+    basic,
+    project,
+    product,
+    core,
+    geologic,
+    image,
+    invertebrate,
+    vertebrate
+  ];
 }
 
-export default { full, basic, project, product, schema, asArray, version };
+export default {
+  full,
+  basic,
+  project,
+  product,
+  core,
+  geologic,
+  image,
+  invertebrate,
+  vertebrate,
+  schema,
+  asArray,
+  version
+};
